b58865b91bc3891b2d812cee0b51c6ead0cccd53
Rahul4dev <rahulsinghsignin@gmail.com>
2023-01-29 23:22:12 +0530

    Added btn and content

---
+++ scripts.js
@@ -0,0 +1,132 @@
+// environment variable
+
+// require('dotenv').config();
+
+// DOM manipulation
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+// out photos will change every time we make request so, global variable
+let photosArray = [];
+
+// Unsplash API
+
+const count = 5;
+// const apiKey = process.env.API_KEY;
+const apiKey = 'xVkS1NHZMO0nZVjWARZFXecqXpu5qBoVgqnvT-k8D2I';
+
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// helper function to set Attributes: following DRY (don't repeat yourself) idea.
+function setAttribute(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+}
+
+// create elements for Links, photos and Add to DOM
+
+function displayPhotos() {
+	// run function forEach method in photosArray
+	photosArray.forEach((photo) => {
+		// create <a> to link to unsplash on blank window
+		const item = document.createElement('a');
+		//item.setAttribute('href', photo.links.html);
+		//item.setAttribute('target', '_blank');  OR
+		setAttribute(item, {
+			href: photo.links.html,
+			target: '_blank',
+		});
+
+		// Create <image></image> for photo and add attribute of src, alt and title
+		const img = document.createElement('img');
+		// img.setAttribute('src', photo.urls.regular);
+		// img.setAttribute('alt', photo.alt_description);
+		// img.setAttribute('title', photo.alt_description);  OR
+		setAttribute(img, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description,
+		});
+
+		// create twitter and Instagram buttons and show Photographer's name
+
+		const photographerDescription = document.createElement('div');
+		const btnContainer = document.createElement('div');
+
+		const twitterBtn = document.createElement('a');
+		const instagramBtn = document.createElement('a');
+		const photographerDetails = document.createElement('a');
+		const photographerName = document.createElement('h6');
+
+		// container attribute: added class to containers
+		setAttribute(photographerDescription, {
+			class: 'photo',
+		});
+
+		setAttribute(btnContainer, {
+			class: 'btn',
+		});
+
+		if (!photo.user.bio) {
+			bio = 'NA';
+		}
+		setAttribute(photographerDetails, {
+			href: photo.user.links.html,
+			target: '_blank',
+			title: photo.user.bio,
+		});
+
+		// buttons attributes: added class to buttons
+		setAttribute(twitterBtn, {
+			class: 'fab fa-twitter',
+			href: `https://twitter.com/${photo.user.social.twitter_username}`,
+			target: '_blank',
+			title: `follow ${photo.user.name} on twitter`,
+		});
+
+		setAttribute(instagramBtn, {
+			class: 'fab fa-instagram',
+			href: `http://instagram.com/${photo.user.social.instagram_username}`,
+			target: '_blank',
+			title: `follow ${photo.user.name} on instagram`,
+		});
+		// Other fields to add like: PhotographerName,
+		setAttribute(photographerName, {
+			class: 'photographerName',
+		});
+
+		photographerName.innerText = photo.user.name;
+		// put <img> inside <a>, <buttons> will be then put both inside our image container element
+		btnContainer.appendChild(twitterBtn);
+		btnContainer.appendChild(instagramBtn);
+		item.appendChild(img);
+		imageContainer.appendChild(item);
+		photographerDescription.appendChild(btnContainer);
+		photographerDetails.appendChild(photographerName);
+		photographerDescription.appendChild(photographerDetails);
+		imageContainer.appendChild(photographerDescription);
+
+		// eventListeners
+
+		// twitterBtn.addEventListener('click', twitterClick);
+		// instagramBtn.addEventListener('click', instagramClick);
+	});
+}
+
+// get photos from unsplash API
+
+async function getPhotos() {
+	try {
+		const response = await fetch(apiUrl);
+		photosArray = await response.json();
+		displayPhotos();
+		console.log(photosArray);
+	} catch (error) {
+		// catch error
+	}
+}
+
+// on load
+
+getPhotos();
